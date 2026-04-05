2b74efb3d6589d592b68958b503417ab4af6c571
Joydeb Choudhury <mcajoydeb@gmail.com>
2026-04-05 23:40:40 +0530

    not found page updated

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,5 +1,16 @@
+import { useLocation, Link } from "react-router-dom";
+
 function NotFound() {
-  return <h2>Not found</h2>;
+  const location = useLocation();
+
+  return (
+    <div>
+      <h2>404 - Page Not Found</h2>
+      <p>Invalid URL: {location.pathname}</p>
+
+      <Link to="/">Go Back Home</Link>
+    </div>
+  );
 }
 
 export default NotFound;
